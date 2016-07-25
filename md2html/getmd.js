77778b791e10a192f76eb57ee2a855cc24496b0e
sujunfei <1982764150@qq.com>
2016-07-25 15:23:53 +0800

    check the crawler

--- md2html/getmd.js
+++ md2html/getmd.js
@@ -1,86 +1,90 @@
-var cron = require('cron').CronJob;
-var superagent = require('superagent');
-var cheerio = require('cheerio');
+var cron = require('cron').CronJob
+var superagent = require('superagent')
+var cheerio = require('cheerio')
 var path = 'https://github.com'
+var rawPath = 'https://raw.githubusercontent.com'
 var fs = require('fs')
 var mongo = require('../model/mongo.js').mongoUse
 var markdown = require('../node_modules/markdown').markdown
 var model = require('../model/schema').models
-  // var blog = new model['Blog']({
-  //   title: title,
-  //   content: blogContent,
-  //   tags:
-  // })
 
+var getFile = {
+  // get the auth-key and cookie_id
+  auth: function() {
+    superagent.get(path + '/login')
+      .end(function(err, res) {
+        if (err)
+          console.log(err)
+        var cookie_id = res.headers['set-cookie'][1].split(';')[0]
+        var $ = cheerio.load(res.text)
+        var auth_key = $('input[name="authenticity_token"]').val()
+        getFile.login(auth_key, cookie_id)
+      })
+  },
+  // login in github 
+  login: function(auth, cookie_id) {
+    superagent.post(path + '/session')
+      .type('form')
+      .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+      .set('Cookie', 'logged_in=no;' + cookie_id + '; _ga=GA1.2.449091290.1464421985; tz=Asia%2FShanghai; _octo=GH1.1.325412906.1464421982; _gat=1;')
+      .send({
+        'commit': 'Sign in',
+        'utf8': '✓',
+        'login': '1982764150@qq.com',
+        'password': 'sjf978977',
+        'authenticity_token': auth
+      })
+      .end(function(err, res) {
+        err ? console.log(err) : getFile.selectMdList()
+      })
+  },
+  // select the md file in github
+  selectMdList: function() {
+    superagent.get(path + '/junfeisu/sjfblog/tree/master/md2html/source')
+      .end(function(err, res) {
+        var md = []
+        var $ = cheerio.load(res.text)
+        var val = $('.js-navigation-open')
+        var date = new Date()
+        for (var i = 2; i < val.length; i++) {
+          md.push(val[i].attribs.href.split('source/')[1]);
+        }
+        for (var j = 0; j < md.length; j++) {
+          if (/^[\u4e00-\u9fa5\w]+\.md$/.test(decodeURI(md[j].split('.md')[0])+'.md')) {
+            console.log(date.getFullYear())
+            console.log(date.getMonth() + 1)
+            var exist = fs.existsSync('./' + date.getFullYear + '-' + date.getMonth() + 1 + '/' + md[j])
+            if (exist) {
+              console.log('the file is exist already')
+            } else {
+              getFile.getContent(md[j])
+            }
+          }else {
+            console.log('there is no new md')
+          }
+        }
+      })
+  },
+  // get the content of md file
+  getContent: function(md) {
+    console.log('213')
+    superagent.get(rawPath + "/junfeisu/sjfblog/master/md2html/source/" + md)
+      .end(function(error, result) {
+        if (err) {
+          console.log('err is ' + error)
+        }
+        var slice = result.text.split('---')
+        var blogMes = {}
+        var date = new Date()
+        var message = slice[1].replace(/\n/g, '')
+        blogMes.tags = message.split('titles: ')[1].split('tags: ')[1]
+        blogMes.title = message.split('titles: ')[1].split('tags: ')[0]
+        blogMes.content = markdown.toHTML(slice[2])
+        fs.writeFileSync('./' + date.getFullYear + '-' + date.getMonth() + 1 + '/' + md, result.text, 'utf-8')
+      })
+  }
+}
 
-var content = fs.readFileSync('./source/苏俊飞.md', 'utf8');
-var a = content.split('---')[1].replace(/\n/g, '');
-var slice = content.split('---');
-var message = slice[1].replace('/\n/g', '');
-var title = message.split('titles: ')[1].split('tags: ')[0];
-var tags = message.split('titles: ')[1].split('tags: ')[1];
-var blogContent = markdown.toHTML(slice[2]);
-
-mongo.add()
-
-new cron('* */10 8-23  * * *', function() {
-  superagent.get(path + '/login')
-    .end(function(err, res) {
-      if (err)
-        console.log(err)
-      var cookie_id = res.headers['set-cookie'][1].split(';')[0];
-      var $ = cheerio.load(res.text);
-      var auth_key = $('input[name="authenticity_token"]').val();
-      superagent.post(path + '/session')
-        .type('form')
-        .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-        .set('Cookie', 'logged_in=no;' + cookie_id + '; _ga=GA1.2.449091290.1464421985; tz=Asia%2FShanghai; _octo=GH1.1.325412906.1464421982; _gat=1;')
-        .send({
-          'commit': 'Sign in',
-          'utf8': '✓',
-          'login': '1982764150@qq.com',
-          'password': 'sjf978977',
-          'authenticity_token': auth_key
-        })
-        .end(function(err, res) {
-          superagent.get(path + '/junfeisu/sjfblog/tree/master/md2html/source')
-            .end(function(err, res) {
-              var md = []
-              var $ = cheerio.load(res.text);
-              var val = $('.js-navigation-open')
-              var date = new Date()
-              for (var i = 2; i < val.length; i++) {
-                md.push(val[i].attribs.href.split('md2html/')[1])
-              }
-              for (var j = 0; j < md.length; j++) {
-                if (/^[a-z]+\.md$/.test(md[j])) {
-                  var exist = fs.existsSync('./' + date.getFullYear() + '-' + date.getMonth() + 1 + '/' + md[j])
-                  var filename = md[j]
-                  if (exist) {
-                    console.log('the file is exist already')
-                    return
-                  }
-                  superagent.get('https://raw.githubusercontent.com/junfeisu/sjfblog/master/md2html/source' + md[j])
-                    .end(function(err, res) {
-                      if (err) {
-                        console.log('err is ' + err);
-                      }
-                      var a = res.text.split('---')[1].replace(/\n/g, '');
-                      var slice = res.text.split('---');
-                      var message = slice[1].replace('/\n/g', '');
-                      var title = message.split('titles: ')[1].split('tags: ')[0];
-                      var tags = message.split('titles: ')[1].split('tags: ')[1];
-                      var blogContent = markdown.toHTML(slice[2]);
-                      fs.writeFileSync('./2016-7/' + filename, res.text, 'utf-8')
-                      console.log('title is ' + title)
-                      console.log('tags is ' + tags)
-                      console.log('blogContent is ' + blogContent)
-                      return
-                    })
-                }
-              }
-              console.log('There is no new md')
-            })
-        })
-    })
-}, null, true, 'Asia/Shanghai');
+// new cron('* */10 8-23  * * *', function() {
+  getFile.auth()
+// }, null, true, 'Asia/Shanghai');
