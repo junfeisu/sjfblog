a2be457e2032f2e2cfdc5dcaa3f095ae2edca336
sujunfei <1982764150@qq.com>
2016-07-27 14:22:35 +0800

    delete the unnecessary file

--- test.js
+++
@@ -1,6 +0,0 @@
-var mongo = require('./model/mongo').mongoUse
-var model = require('./model/schema').models
-
-model['Blog'].aggregate([{$match: { author: 'sujunfei'}}], function(err, res) {
-	err ? console.log('err is ' + err) : console.log(res)
-})
