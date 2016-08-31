809896c3e19fda3dc17eb4d12870ada6cec37e9e
sujunfei <1982764150@qq.com>
2016-08-31 14:54:52 +0800

    prototype blog and add the console style

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import about from './component/about/about'
 import resume from './component/resume/resume'
 import detail from './component/detail/detail'
 
+console.log("%c苏俊飞"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:16em;color:#fff;");
+
 angular.module('app', [uirouter, home, demo, about, resume, detail])
   .config(routing)
   .name
