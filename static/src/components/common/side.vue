cf5997855d938b664dfc5233a89d4b685cece9ce
sjffly <1982764150@qq.com>
2016-10-16 17:21:00 +0800

    finish the side-operate module

--- static/src/components/common/side.vue
+++ static/src/components/common/side.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="side-operate">
-    <div class="concat"></div>
-    <div class="backup"></div>
+    <div class="backup" v-show="isShow" @click="backUp()"></div>
   </div>
 </template>
 
@@ -9,18 +8,62 @@
   export default {
     name: 'sideOperate',
     data () {
-      return {}
+      return {
+        isShow: false
+      }
+    },
+    methods: {
+      watchScroll () {
+        if (navigator.userAgent.toLowerCase().indexOf('firefox') >= 0) {
+          console.log('firefox')
+          window.addEventListener('DOMMouseScroll', event => {
+            document.body.scrollTop > 50 ? this.isShow = true : this.isShow = false
+          })
+        } else {
+          window.onmousewheel = event => {
+            document.body.scrollTop > 50 ? this.isShow = true : this.isShow = false
+          }
+        }
+      },
+      backUp () {
+        console.log('back')
+        let time = setInterval(() => {
+          let scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+          let speed = Math.ceil(scrollTop / 5)
+          document.body.scrollTop = scrollTop - speed
+          if (scrollTop === 0) {
+            clearInterval(time)
+            this.isShow = false
+          }
+        }, 50)
+      }
+    },
+    ready () {
+      this.watchScroll()
     }
   }
 </script>
 
 <style lang="scss">
+  @import './../../assets/style/mixin.scss';
   .side-operate {
     position: fixed;
     bottom: 20px;
     right: 100px;
     width: 50px;
-    min-height: 50px;
-    background-color: red;
+    background-color: #999;
+  }
+  .backup {
+    background: url('../../assets/img/backUp.png');
+    background-size: 50%;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    height: 50px;
+    cursor: pointer;
+    opacity: 0.5;
+    &:hover {
+      opacity: 1;
+      @include transition($time: 0.5s);
+    }
   }
 </style>
