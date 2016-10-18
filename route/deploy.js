b5736ad54d773650c503d5d48e3c645482e02033
sjffly <1982764150@qq.com>
2016-10-18 17:22:25 +0800

    def

--- route/deploy.js
+++ route/deploy.js
@@ -1,8 +1,7 @@
 var express = require('express')
 var route = express.Router()
 var exec = require('child_process').exec
-var getMd = require('./../md2html/getmd').getFile
-
+var getMd = require('./../md2html/getmd')
 route.post('/', function (req, res) {
   exec('git pull', {'cwd': '/home/www/sjfblog'}, function (err, stdout, stderr) {
     if (err !== null) {
